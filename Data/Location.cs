805fae83fb05510b341214efa6535029f30f5aea
emily <emily.coate@gmail.com>
2016-04-16 08:39:38 -0400

    fixing android manifest and adding the locations class

---
+++ Data/Location.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Data
+{
+    public class Location
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string category { get; set; }
+        public string address { get; set; }
+        public string phone { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public object email { get; set; }
+        public string website { get; set; }
+        public string description { get; set; }
+        public int visitCount { get; set; }
+
+    }
+}
